a5a9c9dc87d394e9fb8d623cbb651527fdb2e4d2
wangboc <baixibao@gmail.com>
2018-10-12 16:27:01 +0800

    Bug fixed

--- Classification.m
+++ Classification.m
@@ -41,7 +41,7 @@ LMCI_vs_AD   = cat(1, Subject_LMCI, Subject_AD);
 [FilteredMatrix, FilterdIndex] = Filter_Feature_Rank_importance(HC_vs_LMCI, 1/2);
 %% Wrapper Feature selection
 [Selected_train_data, SelectedFeatures_in_RankImportanceOrder] = WrapperFeatureSelection(FilteredMatrix, 0);
-RankImportanceOrder_2_FeatureName(SelectedFeatures_in_RankImportanceOrder);
+RankImportanceOrder_2_FeatureName(FilterdIndex, SelectedFeatures_in_RankImportanceOrder);
 %% Matlab Machine learning Toolbox ...
 %% libSVM tools
 libSVM_result_filename = 'tempfiles\libSVM_result.txt';
